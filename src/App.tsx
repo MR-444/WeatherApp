b4aebe2984326f8dec2589d1197647f4bc454a50
Markus R <m.reinke@fielmann.com>
2019-07-19 18:33:19 +0200

    Weekly overview as function

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,46 @@ import {DayOfWeek} from  "./models/DayOfWeek";
 
 
 const App: React.FC = () => {
-  const weeklyWeatherArray = new WeatherData().getWeeklyData();
-
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Your daily weather forecast of the next week in celsius
+          <Clock date={new Date()}/>
+          Your daily weather forecast of the next week in celsius.
         </p>
-        <div>
-          Monday:    {weeklyWeatherArray[DayOfWeek.Monday].temp} <br />
-
-          Tuesday:   {weeklyWeatherArray[DayOfWeek.Tuesday].temp} <br />
-
-          Wednesday: {weeklyWeatherArray[DayOfWeek.Wednesday].temp} <br />
-
-          Thursday:  {weeklyWeatherArray[DayOfWeek.Thursday].temp} <br />
-
-          Friday:    {weeklyWeatherArray[DayOfWeek.Friday].temp} <br />
-
-          Saturday:  {weeklyWeatherArray[DayOfWeek.Saturday].temp} <br />
-
-          Sunday:    {weeklyWeatherArray[DayOfWeek.Sunday].temp} <br />
-        </div>
+        <Overview/>
       </header>
+    </div>
+  );
+}
+
+function Clock(props: any) {
+  return (
+    <div>
+      <h5>It is {props.date.toLocaleTimeString()}.</h5>
+    </div>
+  );
+}
+
+
+function Overview() {
+  const weeklyWeatherArray = new WeatherData().getWeeklyData();
+
+  return (
+    <div>
+      Monday:    {weeklyWeatherArray[DayOfWeek.Monday].temp} <br />
+
+      Tuesday:   {weeklyWeatherArray[DayOfWeek.Tuesday].temp} <br />
+
+      Wednesday: {weeklyWeatherArray[DayOfWeek.Wednesday].temp} <br />
+
+      Thursday:  {weeklyWeatherArray[DayOfWeek.Thursday].temp} <br />
+
+      Friday:    {weeklyWeatherArray[DayOfWeek.Friday].temp} <br />
+
+      Saturday:  {weeklyWeatherArray[DayOfWeek.Saturday].temp} <br />
+
+      Sunday:    {weeklyWeatherArray[DayOfWeek.Sunday].temp} <br />
     </div>
   );
 }
